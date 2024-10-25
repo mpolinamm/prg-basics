ca71531cfa8882bad4ca1dbf081b2a92d77399e6
Polina Minkova <minkovapmpm@gmail.com>
2024-10-25 11:20:58 +0200

    second and third parts completed

---
+++ 04-Functions/2-1.py
@@ -0,0 +1,29 @@
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+letter = input('Enter your letter: ')
+print(letter)
+
+number = '20303'
+new_number = int(number)
+print(new_number)
+
+decimal_number = 304
+binary_number = bin(decimal_number)
+print(binary_number)
+
+decimal_number1 = 304
+hexadecimal_number = hex(decimal_number1)
+print(hexadecimal_number)
+
+print(f'€ - {ord("€")}')
+
+number = -17
+absolute_value = abs(number)
+print(absolute_value)
---
+++ 04-Functions/3-1.py
@@ -0,0 +1,10 @@
+import math
+
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
+
+natural_logarithm = math.log1p(5)
+print('A natural logarithm of 5 is', natural_logarithm)
+
+sin = math.sin(90)
+print('A sine of 90 degrees is', sin)
