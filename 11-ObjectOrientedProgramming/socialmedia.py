9a96e0f203ebc73f9c6b407653762aa636aa7b46
Polina Minkova <minkovapmpm@gmail.com>
2024-12-20 12:24:53 +0100

    cw

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,15 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def display_timeline(self):
+        print(f"Timeline for {self.username}:")
+        for index, post in enumerate(self.posts, 1):
+            print(f"{index}. {post}")
+
+johndoe = SocialMediaProfile("johndoe")
+
+johndoe.add_post("Hello, world!")
+johndoe.add_post("Had a great day at the park!")
+johndoe.add_post("What's up, Natalie? How are you?")
+
+johndoe.display_timeline()
